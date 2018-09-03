d9c7f74a145789c4cc78ac05924fde983e72f6e7
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-03 23:45:27 +0200

    Initial commit

---
+++ src/main/java/pl/iridium405/s_h_project/SHProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.iridium405.s_h_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SHProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SHProjectApplication.class, args);
+    }
+}
