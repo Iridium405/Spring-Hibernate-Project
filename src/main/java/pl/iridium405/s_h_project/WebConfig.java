87dc1bdded3b53e6b88e68b638194a555d944af5
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-07 12:30:42 +0200

    Layout basic construction created

---
+++ src/main/java/pl/iridium405/s_h_project/WebConfig.java
@@ -0,0 +1,26 @@
+package pl.iridium405.s_h_project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/img/**",
+                "/css/**",
+                "/js/**",
+                "/bootstrap/**",
+                "/jquery/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/",
+                        "classpath:/static/bootstrap/",
+                        "classpath:/static/jquery/");
+    }
+
+}
