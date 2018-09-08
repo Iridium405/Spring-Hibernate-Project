6f3c03aee1b29e8d798f9d4266810e1f913427ef
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-08 13:04:42 +0200

    Mid-changes

--- src/main/java/pl/iridium405/twitter_like/WebConfig.java
+++ src/main/java/pl/iridium405/twitter_like/WebConfig.java
@@ -7,20 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(
-                "/img/**",
-                "/css/**",
-                "/js/**",
-                "/bootstrap/**",
-                "/jquery/**")
-                .addResourceLocations(
-                        "classpath:/static/img/",
-                        "classpath:/static/css/",
-                        "classpath:/static/js/",
-                        "classpath:/static/bootstrap/",
-                        "classpath:/static/jquery/");
-    }
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler(
+                    "/webjars/**",
+                    "/img/**",
+                    "/css/**",
+                    "/js/**")
+                    .addResourceLocations(
+                            "classpath:/META-INF/resources/webjars/",
+                            "classpath:/static/img/",
+                            "classpath:/static/css/",
+                            "classpath:/static/js/");
+        }
+
 
 }
